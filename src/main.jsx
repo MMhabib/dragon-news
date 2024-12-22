bd7712cd0e63fd68b6637774b8ec902ae3ac6c34
Habib <habibleo861@gmail.com>
2024-12-22 14:42:43 +0600

    Authprovider

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import routes from './routes/Routes'
-import { RouterProvider } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import routes from "./routes/Routes";
+import { RouterProvider } from "react-router-dom";
+import Authprovider from "./providers/Authprovider";
 
-
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-   <RouterProvider router={routes}/>
-  </StrictMode>,
-)
+    <Authprovider>
+      <RouterProvider router={routes} />
+    </Authprovider>
+  </StrictMode>
+);
