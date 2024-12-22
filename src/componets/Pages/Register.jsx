27692939e20b6f30a27b9b9a013f501ed8b50490
Habib <habibleo861@gmail.com>
2024-12-22 15:15:16 +0600

    register

--- src/componets/Pages/Register.jsx
+++ src/componets/Pages/Register.jsx
@@ -1,92 +1,112 @@
 import { Link } from "react-router-dom";
 import Navbar from "./Navbar";
-
+import { useContext } from "react";
+import { AuthContext } from "../../providers/Authprovider";
 
 const Register = () => {
+  const { createUser } = useContext(AuthContext);
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const form = new FormData(e.currentTarget);
+    const email = form.get("email");
+    const name = form.get("name");
+    const url = form.get("photo-url");
+    const password = form.get("password");
 
-    const handleRegister=(e)=>{
-        e.preventDefault();
-        const form=new FormData(e.currentTarget);
-        const email=form.get('email');
-        const name=form.get('name');
-        const url=form.get('photo-url');
-        const password=form.get('password');
-console.log(email,name,password,url);
-          }
-          return (
-            <div className="mt-10">
-              <Navbar></Navbar>
-        
-              <div className="hero-content flex-col lg:flex-row-reverse my-10 p-16">
-                <div className="bg-slate-50 w-[752px] h-[700px] drop-shadow-md  flex justify-center  items-center">
-                  <div className=" w-[606px] h-[548px] ">
-                  <div className="border-b-2">
-                    <h1 className="text-4xl text-center font-semibold mb-4">Register account</h1>
-                    </div>
-                  <form onSubmit={handleRegister} className=" w-[559px] h-[339px] m-6">
-                  <div className="form-control w-full">
-                      <label className="label">
-                        <span className="label-text">Name</span>
-                      </label>
-                      <input
-                        type="text"
-                        name="name"
-                        placeholder="Your name"
-                        className="input input-bordered"
-                        required
-                      />
-                    </div>
-                    <div className="form-control w-full">
-                      <label className="label">
-                        <span className="label-text">Email</span>
-                      </label>
-                      <input
-                        type="email"
-                        name="email"
-                        placeholder="email"
-                        className="input input-bordered"
-                        required
-                      />
-                    </div>
-                   
-                    <div className="form-control w-full">
-                      <label className="label">
-                        <span className="label-text">Photo Url</span>
-                      </label>
-                      <input
-                        type="text"
-                        name="photo-url"
-                        placeholder="Photo Url"
-                        className="input input-bordered"
-                        required
-                      />
-                    </div>
-                    <div className="form-control">
-                      <label className="label">
-                        <span className="label-text">Password</span>
-                      </label>
-                      <input
-                        type="password"
-                        placeholder="password"
-                        className="input input-bordered"
-                        required
-                      />
-                      <label className="label">
-                        <a href="#" className="label-text-alt link link-hover">
-                          Forgot password?
-                        </a>
-                      </label>
-                    </div>
-                    <div className="form-control mt-6">
-                      <button className="btn btn-primary">Register</button>
-                    </div>
-                  </form>
-                  <p className="text-sm text-center">Already have account?<Link className="text-red-700" to='/login'>Log In</Link></p>
-                  </div>
-                </div>
-              </div>
+    createUser(email, password)
+      .then((result) => {
+        console.log(result.user);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  return (
+    <div className="mt-10">
+      <Navbar></Navbar>
+
+      <div className="hero-content flex-col lg:flex-row-reverse my-10 p-16">
+        <div className="bg-slate-50 w-[752px] h-[700px] drop-shadow-md  flex justify-center  items-center">
+          <div className=" w-[606px] h-[548px] ">
+            <div className="border-b-2">
+              <h1 className="text-4xl text-center font-semibold mb-4">
+                Register account
+              </h1>
             </div>
-          );
-        };
+            <form
+              onSubmit={handleRegister}
+              className=" w-[559px] h-[339px] m-6"
+            >
+              <div className="form-control w-full">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Your name"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control w-full">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+
+              <div className="form-control w-full">
+                <label className="label">
+                  <span className="label-text">Photo Url</span>
+                </label>
+                <input
+                  type="text"
+                  name="photo-url"
+                  placeholder="Photo Url"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  name='password'
+                  placeholder="password"
+                  className="input input-bordered"
+                  required
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Register</button>
+              </div>
+            </form>
+            <p className="text-sm text-center">
+              Already have account?
+              <Link className="text-red-700" to="/login">
+                Log In
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Register;
--- src/providers/Authprovider.jsx
+++ src/providers/Authprovider.jsx
@@ -1,29 +1,28 @@
 import { createContext, useState } from "react";
-import auth from './../componets/Firebase/firebase.config';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import app from './../componets/Firebase/firebase.config';
 
+export const AuthContext = createContext(null);
 
-export const AuthContext=createContext(null);
+const auth = getAuth(app);
 
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
 
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
 
-const Authprovider = ({children}) => {
-const [user,setUser]=useState(null);
-
-const createUSer=(email,password)=>{
-    return createUserWithEmailAndPassword(auth,email,password)
-}
-
-
-const authInfo={
+  const authInfo = {
     user,
-    createUSer
-}
+    createUser,
+  };
 
-    return (
-        <AuthContext.Provider value={authInfo}>
-            {children}
-        </AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider value={authInfo}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
-export default Authprovider;
+export default AuthProvider;
