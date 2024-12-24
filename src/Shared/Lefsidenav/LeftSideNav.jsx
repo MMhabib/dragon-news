4af6c48e68c7c03032c7ef9e6145834541eb4fe7
Habib <habibleo861@gmail.com>
2024-12-24 18:07:52 +0600

    leftsidenav

--- src/Shared/Lefsidenav/LeftSideNav.jsx
+++ src/Shared/Lefsidenav/LeftSideNav.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftSideNav = () => {
+const[categories,setCategories]=useState([])
+useEffect(()=>{
+    fetch('categories.json')
+    .then(res=>res.json())
+    .then(data=>setCategories(data))
+},[])
+
     return (
         <div>
             <h2 className="text-3xl">All catergory</h2>
             <div >
                 <h1 className='text-center font-semibold text-xl bg-[#E7E7E7] min-w-64 h-16 flex items-center justify-center mt-4 rounded-md'>National News</h1>
 
-                
+                {
+                    categories.map(category=><Link className='block ml-16 text-xl mt-8 text-[#9F9F9F] font-medium' key={category.id}>{category.name}</Link>)
+                }
             </div>
         </div>
     );
